0342353965c03947a52814de9717e5b2764e1842
Илья Лебедев <inl@MacBook-Air-Ila.local>
2025-03-26 00:41:50 +0300

    first commit

---
+++ tests/testsApp.swift
@@ -0,0 +1,17 @@
+//
+//  testsApp.swift
+//  tests
+//
+//  Created by Илья Лебедев on 26.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct testsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
